5898e7a9dfde03b9431b816286b5b6802b776a44
ronen <narkisr@gmail.com>
2015-01-25 14:28:24 +0200

    cleanup and latest plugins

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,13 +2,13 @@ group{ 'puppet': ensure  => present }
 
 node 'jenkins.local' {
   include jenkins
-  jenkins::plugin {'git' : version => '1.1.26' }
-  jenkins::plugin {'mercurial' : version => '1.42' }
-  jenkins::plugin {'gradle' : version => '1.21' }
-
-  package{['mercurial','git']:
-    ensure  => present
-  }
   include jdk
   include build::gradle
+  jenkins::plugin {'git' : version => '2.3.4' }
+  jenkins::plugin {'gradle' : version => '1.24' }
+
+  package{['git']:
+    ensure  => present
+  }
+
 }
