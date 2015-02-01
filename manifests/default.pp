d87ef41cfc95a2f8a5c5a53e2694e593df5be036
ronen <narkisr@gmail.com>
2015-02-01 17:54:47 +0200

    installing all deps

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,7 +4,11 @@ node 'jenkins.local' {
   include jenkins
   include jdk
   include build::gradle
-  jenkins::plugin {'git' : version => '2.3.4' }
+
+  jenkins::plugin {['git','credentials','git-client',
+                      'scm-api', 'matrix-project','token-macro',
+                      'ssh-credentials', 'mailer']: }
+
   jenkins::plugin {'gradle' : version => '1.24' }
 
   package{['git']:
