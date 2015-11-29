180d1ba705b18dbd1276efdbffc4778cb5b62901
ronen <narkisr@gmail.com>
2015-11-29 17:46:58 +0200

    jdk 8 and ppa fix

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,18 +1,26 @@
-group{ 'puppet': ensure  => present }
+node default {
 
-node 'jenkins.local' {
-  include jenkins
-  include jdk
+  class{'jenkins':
+    install_java => false
+  }
+
+  class{'jdk':
+    version => 8
+  }
+
   include build::gradle
 
   jenkins::plugin {['git','credentials','git-client',
                       'scm-api', 'matrix-project','token-macro',
                       'ssh-credentials', 'mailer']: }
-
+  
   jenkins::plugin {'gradle' : version => '1.24' }
 
-  package{['git']:
+  package{ 'git':
     ensure  => present
   }
 
+  package{ ['python-software-properties', 'software-properties-common']:
+    ensure  => present
+  } -> Exec <||>
 }
