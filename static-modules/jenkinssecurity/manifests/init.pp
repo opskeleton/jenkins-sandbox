ccbfd05c5bcf0d9f372d2e515eae229c62fc92d2
ronen <narkisr@gmail.com>
2015-12-01 02:12:35 +0200

    adding users

--- static-modules/jenkinssecurity/manifests/init.pp
+++ static-modules/jenkinssecurity/manifests/init.pp
@@ -1,11 +1,12 @@
 # Secured jenkins instance
-class jenkinssecurity {
+class jenkinssecurity($user_hash = {}) {
 
   include jenkinssecurity::nginx
   include jenkinssecurity::jvm
 
   class{'jenkins':
-    install_java => false
+    install_java => false,
+    user_hash    => $user_hash
   }
 
 }
